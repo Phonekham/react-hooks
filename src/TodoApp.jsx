b55cde8fe8a97a30b926f5bae50ad2592eacefc4
Phonekham <phone.madridista@gmail.com>
2019-10-08 14:10:01 +0700

    LocalStorage w UseEffect Hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -10,12 +10,17 @@ import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 function TodoApp() {
-  const initTodos = [
-    { id: 1, task: "feed cat", completed: false },
-    { id: 2, task: "code", completed: true },
-    { id: 3, task: "shopping", completed: false }
-  ];
+  const initTodos = JSON.parse(window.localStorage.getItem("todos") || "[] ");
+  // const initTodos = [
+  //   { id: 1, task: "feed cat", completed: false },
+  //   { id: 2, task: "code", completed: true },
+  //   { id: 3, task: "shopping", completed: false }
+  // ];
   const [todos, setTodos] = useState(initTodos);
+
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = newTodoText => {
     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
